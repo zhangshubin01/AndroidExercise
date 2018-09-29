12924d5de0ff3a045ad957dafd5299fffcc36726
张淑宾 <66003549@163.com>
2018-09-29 16:32:23 +0800

    提交

---
+++ recyclerviewlibrary/src/main/java/com/padtast/recyclerviewlibrary/paymentrecord/activity/PaymentRecordActivity.java
@@ -0,0 +1,15 @@
+package com.padtast.recyclerviewlibrary.paymentrecord.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.padtast.recyclerviewlibrary.R;
+
+public class PaymentRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_record);
+    }
+}
